1c90dfb69a85ce691466083c9ef74905180d8ceb
Michele Caprotti <73969179+caprosoft@users.noreply.github.com>
2025-10-28 12:38:33 +0100

    Update Odoo installation method in almalinux9.sh
    Replaced Odoo installation method with wget and localinstall.

--- almalinux9.sh
+++ almalinux9.sh
@@ -11,7 +11,10 @@ sudo postgresql-setup --initdb --unit postgresql
 sudo systemctl enable postgresql
 sudo systemctl start postgresql
 
-sudo dnf config-manager --add-repo=https://nightly.odoo.com/19.0/nightly/rpm/odoo.repo
-sudo dnf install -y odoo
+#sudo dnf config-manager --add-repo=https://nightly.odoo.com/19.0/nightly/rpm/odoo.repo
+#sudo dnf install -y odoo
+wget https://nightly.odoo.com/19.0/nightly/rpm/odoo_19.0.latest.noarch.rpm
+sudo dnf localinstall odoo_19.0.latest.noarch.rpm
+rm odoo_19.0.latest.noarch.rpm
 sudo systemctl enable odoo
 sudo systemctl start odoo
