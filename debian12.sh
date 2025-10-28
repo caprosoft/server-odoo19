d7bae08979acdf68219926897ca957e6cedc0118
Michele Caprotti <73969179+caprosoft@users.noreply.github.com>
2025-10-28 12:08:43 +0100

    Add sudo to package management commands in script

--- debian12.sh
+++ debian12.sh
@@ -1,21 +1,15 @@
 #!/bin/sh
 
-apt update -y && apt upgrade -y
-
-apt install wget -y
+sudo apt update -y && sudo apt upgrade -y
 
 wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-3/wkhtmltox_0.12.6.1-3.bookworm_amd64.deb
-
-dpkg -i wkhtmltox_0.12.6.1-3.bookworm_amd64.deb
-apt-get install -f -y
-dpkg -i wkhtmltox_0.12.6.1-3.bookworm_amd64.deb
-
+sudo dpkg -i wkhtmltox_0.12.6.1-3.bookworm_amd64.deb
+sudo apt-get install -f -y
+sudo dpkg -i wkhtmltox_0.12.6.1-3.bookworm_amd64.deb
 rm wkhtmltox_0.12.6.1-3.bookworm_amd64.deb
 
-apt install postgresql -y
+sudo apt install postgresql -y
 
-apt install gpg -y
-
-wget -q -O - https://nightly.odoo.com/odoo.key | gpg --dearmor -o /usr/share/keyrings/odoo-archive-keyring.gpg
-echo 'deb [signed-by=/usr/share/keyrings/odoo-archive-keyring.gpg] https://nightly.odoo.com/19.0/nightly/deb/ ./' | tee /etc/apt/sources.list.d/odoo.list
-apt-get update && apt-get install odoo
+wget -q -O - https://nightly.odoo.com/odoo.key | sudo gpg --dearmor -o /usr/share/keyrings/odoo-archive-keyring.gpg
+echo 'deb [signed-by=/usr/share/keyrings/odoo-archive-keyring.gpg] https://nightly.odoo.com/19.0/nightly/deb/ ./' | sudo tee /etc/apt/sources.list.d/odoo.list
+sudo apt-get update && sudo apt-get install odoo
