c8d9bb8680c21ad11181cf54eb34475262ddfadf
Michele Caprotti <73969179+caprosoft@users.noreply.github.com>
2025-10-28 12:05:32 +0100

    Update AlmaLinux script for Odoo installation

---
+++ almalinux9.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+sudo dnf upgrade -y
+
+wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-3/wkhtmltox-0.12.6.1-3.almalinux9.x86_64.rpm
+sudo dnf localinstall wkhtmltox-0.12.6.1-3.almalinux9.x86_64.rpm
+rm wkhtmltox-0.12.6.1-3.almalinux9.x86_64.rpm
+
+sudo dnf install -y postgresql-server
+sudo postgresql-setup --initdb --unit postgresql
+sudo systemctl enable postgresql
+sudo systemctl start postgresql
+
+sudo dnf config-manager --add-repo=https://nightly.odoo.com/19.0/nightly/rpm/odoo.repo
+sudo dnf install -y odoo
+sudo systemctl enable odoo
+sudo systemctl start odoo
