cd7e32375feac24bb8fc21fa7f577e708bc352a3
Michele Caprotti <73969179+caprosoft@users.noreply.github.com>
2025-10-28 12:17:32 +0100

    Add -y flag to localinstall command for wkhtmltox

--- almalinux9.sh
+++ almalinux9.sh
@@ -3,7 +3,7 @@
 sudo dnf upgrade -y
 
 wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-3/wkhtmltox-0.12.6.1-3.almalinux9.x86_64.rpm
-sudo dnf localinstall wkhtmltox-0.12.6.1-3.almalinux9.x86_64.rpm
+sudo dnf localinstall wkhtmltox-0.12.6.1-3.almalinux9.x86_64.rpm -y
 rm wkhtmltox-0.12.6.1-3.almalinux9.x86_64.rpm
 
 sudo dnf install -y postgresql-server
